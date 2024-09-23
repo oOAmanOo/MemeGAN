3a627c5e0e4c39c5adf1992ad0f3ecdee93f054f
oOAmanOo <iamamanda021024@gmail.com>
2024-09-24 04:47:14 +0800

    refactor: add LLM(2 possible options)

--- Main/extractor.py
+++ Main/extractor.py
@@ -1,8 +1,4 @@
 import torch.nn as nn
-import pandas as pd
-from torch.distributed.pipelining import pipeline
-
-from transformers import pipeline
 from transformers import AutoTokenizer
 from tqdm import tqdm
 from transformers import AutoImageProcessor, Swinv2Model
@@ -14,25 +10,35 @@ def addImagePath(data, imgPath):
     return data
 
 def textExtraction(text_data):
-    # 載入模型
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    ##### google/gemma-2b #####
+    # tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+    # vocab_size = 256128  # 词汇表大小
 
-    pipe = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-emoji", device=0)
+    ###### twitter-roberta-base-emoji ######
     tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-emoji")
-    vocab_size = 50265  # 词汇表大小
-    embedding_dim = 768  # 嵌入维度，与你的图像嵌入维度相同
-    text_embedding = nn.Embedding(vocab_size, embedding_dim).to(device)
+    vocab_size = 50265  # 詞彙表大小
+
+    embedding_dim = 768  # 嵌入维度，與你的圖片嵌入维度相同
+    text_embedding = nn.Embedding(vocab_size, embedding_dim)
 
     all_features = []
     with tqdm(text_data) as pbar:
         for text in (text_data):
             tokens = tokenizer(text, truncation=True, padding='max_length', max_length=373, return_tensors='pt')
-            output = text_embedding(tokens['input_ids'].to(device))
-            output = output.cpu()
+            output = text_embedding(tokens['input_ids'])
             all_features.append(output)
             pbar.update(1)
         return torch.cat(all_features)
 
+def textExtractReverse(data):
+    ##### google/gemma-2b #####
+    # tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+
+    ###### twitter-roberta-base-emoji ######
+    tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-emoji")
+
+    indices = tokenizer.batch_decode(data.squeeze(-1), skip_special_tokens=True)
+    return indices
 
 # 定義批量處理和提取特徵的函數
 def imageExtraction(image_data):
@@ -60,7 +66,6 @@ def imageExtraction(image_data):
                 # 獲取 Swinv2 模型的輸出
                 outputs = swin(**inputs)
                 last_hidden_states = outputs.last_hidden_state
-                # print(last_hidden_states.shape)
                 # 儲存特徵
                 last_hidden_states = last_hidden_states.cpu()
                 all_features.append(last_hidden_states)
